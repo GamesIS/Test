332c1b3250837a4d958db85c174fa3d66c7deb33
ilyak1997 <IlyaK1997@gmail.com>
2017-04-18 14:57:49 +0400

    NewBranch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Master");
+        System.out.println("Master");
+        System.out.println("Master");
+        System.out.println("Master");
+        System.out.println("Master");
+        System.out.println("Master");
+        System.out.println("Master");
+        System.out.println("Master");
+        System.out.println("Master");
+        System.out.println("Master");
+        System.out.println("Master");
+        System.out.println("Master");
+        System.out.println("Master");
+        System.out.println("Master");
+        System.out.println("Master");
+        System.out.println("Master");
+
     }
 }
